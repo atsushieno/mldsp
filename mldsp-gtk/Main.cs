8d1c50a30c486d6b112557f3c6951bae16d05ca9
Atsushi Eno <atsushi@linux-q4dp.site>
2009-06-03 02:27:03 +0900

    copy xaml to exec directory. Use moonlight.gtk.MoonlightHost. not verified the code yet.

--- mldsp-gtk/Main.cs
+++ mldsp-gtk/Main.cs
@@ -8,8 +8,17 @@ namespace mldspgtk
 		public static void Main (string[] args)
 		{
 			Application.Init ();
-			MainWindow win = new MainWindow ();
-			win.Show ();
+			//MainWindow win = new MainWindow ();
+			//win.Show ();
+			Window w = new Window ("mldsp");
+			w.DefaultHeight = 600;
+			w.DefaultWidth = 800;
+			w.DeleteEvent += delegate { Application.Quit (); };
+			var moon = new Moonlight.Gtk.MoonlightHost ();
+			moon.LoadXamlFromFile ("App.xaml");
+			w.Add (moon);
+			w.ShowAll ();
+
 			Application.Run ();
 		}
 	}
