19c4b2b468d36414c191ee5f357291a4f2fa9ca4
Atsushi Eno <atsushi@linux-avjl.(none)>
2009-06-14 05:16:47 +0900

    remove Encoding.ASCII.

--- mldsp/SMF.cs
+++ mldsp/SMF.cs
@@ -320,7 +320,7 @@ namespace Commons.Music.Midi
 
 		public void WriteHeader (short format, short tracks, short deltaTimeSpec)
 		{
-			stream.Write (Encoding.ASCII.GetBytes ("MThd"), 0, 4);
+			stream.Write (Encoding.UTF8.GetBytes ("MThd"), 0, 4);
 			WriteShort (0);
 			WriteShort (6);
 			WriteShort (format);
@@ -330,7 +330,7 @@ namespace Commons.Music.Midi
 
 		public void WriteTrack (SmfTrack track)
 		{
-			stream.Write (Encoding.ASCII.GetBytes ("MTrk"), 0, 4);
+			stream.Write (Encoding.UTF8.GetBytes ("MTrk"), 0, 4);
 			WriteInt (GetTrackDataSize (track));
 
 			byte running_status = 0;
